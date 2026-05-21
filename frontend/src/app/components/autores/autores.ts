6cc017a770fb3fb0e399941d5570c93ceddf853a
jesusrincon98 <jesusangelrincon98@gmail.com>
2026-05-21 12:57:52 +0200

    Añadir formulario reactivo para crear autores

--- frontend/src/app/components/autores/autores.ts
+++ frontend/src/app/components/autores/autores.ts
@@ -1,24 +1,50 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { LibroService } from '../../services/libro';
-import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-autores',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './autores.html',
   styleUrl: './autores.css'
 })
 export class AutoresComponent implements OnInit {
 
   autores = signal<any[]>([]);
+  mostrarFormulario = signal(false);
+
+  formulario = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    nacionalidad: new FormControl('', Validators.required),
+    edad: new FormControl('', Validators.required),
+    biografia: new FormControl('')
+  });
 
   constructor(private libroService: LibroService) {}
 
   ngOnInit() {
+    this.cargarAutores();
+  }
+
+  cargarAutores() {
     this.libroService.getAutores().subscribe(data => {
       this.autores.set(data);
     });
+  }
+
+  toggleFormulario() {
+    this.mostrarFormulario.set(!this.mostrarFormulario());
+  }
+
+  crearAutor() {
+    if (this.formulario.valid) {
+      this.libroService.crearAutor(this.formulario.value).subscribe(() => {
+        this.cargarAutores();
+        this.formulario.reset();
+        this.mostrarFormulario.set(false);
+      });
+    }
   }
 }
